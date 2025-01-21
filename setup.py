ba7d271b9b73fc15e9e8a88bda26c2f03130a715
Burak Cantürk <burakcanturk12@gmail.com>
2025-01-21 14:30:41 +0300

    xyz

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ import os
 
 setup(
     name="gcd-lcm-calculator",  # Projenizin adı (benzersiz olmalı)
-    version="1.0.0",  # Versiyon numarası
+    version="1.0.1",  # Versiyon numarası
     author="Burak Cantürk",
     author_email="burakcanturk12@gmail.com",
-    description="This is a library for Quectel Modules.",
+    description="You can find the gcm (greatest common divisior) and lcm (least command multiplier) of the numbers. And also you can find the other values of the primity.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/burakcanturk/Quectel-5G-LTE-Module-Library.git",
